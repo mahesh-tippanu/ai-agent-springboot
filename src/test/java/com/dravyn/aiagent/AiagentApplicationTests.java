ee08ea259db993808d6ed4b2d8d65b784341d8c5
Mahesh Kumar Tippanu <mtippanu@student.gitam.edu>
2025-07-28 22:45:35 +0530

    Initial Spring Boot AI Agent setup

---
+++ src/test/java/com/dravyn/aiagent/AiagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dravyn.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
